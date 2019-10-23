047001fa075e6d000c98e7c4654a1dc07479d59c
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-23 12:54:50 +0200

    Add project files.

---
+++ ScontoSupermercato6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScontoSupermercato6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inserisci l'importo totale");
+            double importo = double.Parse(Console.ReadLine());
+            double importotot = 0;
+            double sconto = 0;
+            if (importo < 50)
+            {
+                sconto = importo * 5 / 100;
+                importotot = importo - sconto;
+                Console.WriteLine($"l'importo scontato è {importotot:c}");
+            }
+            else if (importo > 50 && importo < 65 )
+            {
+                sconto = importo * 6 / 100;
+                importotot = importo - sconto;
+                Console.WriteLine($"l'importo scontato è {importotot:c}");
+            }
+            else if (importo > 65 && importo < 80)
+            {
+                sconto = importo * 7 / 100;
+                importotot = importo - sconto;
+                Console.WriteLine($"l'importo scontato è {importotot:c}");
+            }
+            else if (importo > 80)
+            {
+                sconto = importo * 10 / 100;
+                importotot = importo - sconto;
+                Console.WriteLine($"l'importo scontato è {importotot:c}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
